73495621cd5e07217720c8e61f4240627f71b791
CodeGawd914 <Travisgdesir31@gmail.com>
2019-03-06 11:15:56 -0500

    finished the clock

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 console.log("im ready");
+const secondHand = document.querySelector('.second-hand')
+const minHand = document.querySelector('.min-hand')
+const hourHand = document.querySelector('.hour-hand')
 
 function setTic() {
   const now = new Date()
+
   const seconds = now.getSeconds()
-  console.log(seconds);
+  const secondDegree = ((seconds/60)* 360)+90
+  secondHand.style.transform = `rotate(${secondDegree}deg)`
+
+  const mins = now.getMinutes()
+  const minsDegree = ((mins/60)* 360)+90
+  minHand.style.transform = `rotate(${minsDegree}deg)`
+
+  const hour = now.getMinutes()
+  const hourDegree = ((hour/12)* 360)+90
+  hourHand.style.transform = `rotate(${hourDegree}deg)`
 }
 
 setInterval(setTic,1000)
